b6dbd19665d0b7e7dd10119b69421798aa56a5ae
Hhardi <vigurk@gmail.com>
2015-09-16 15:21:58 +0300

    Failide lisamine

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php header('Content-Type: text/html; charset=UTF-8'); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+    <meta charset="UTF-8">
+    <title>Richard Bota</title>
+</head>
+<body>
+<div class="h"><h1>Richard</h1></div>
+<ul>
+    <li>27 aastat vana</li>
+    <li>Lõpetas Raatuse Gümnaasiumi</li>
+    <li>Lõpetas Maaülikooli ökonoomika erialal</li>
+</ul>
+<img id= "kool" src="http://tny.im/2FP"/>
+<div class="h"><h2>Hobid</h2></div>
+<p>Tegeleb aktiivselt spordiga</p>
+<hr>
+<p>crossfitt, ujumine,bodybump</p>
+<p>Meeldib samuti rattaga sõita, rulluisutada, suusatada</p>
+<table>
+    <thead>
+    <tr>
+        <th colspan="3"></th>  <!-- tulpade arv -->
+    </tr>
+    <tr>
+        <th>Ettevõtte</th>
+        <th>Amet</th>
+        <th>Töö pikkus</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Arco Vara</td>
+        <td>kinnisvaramaakler</td>
+        <td>6 kuud</td>
+    </tr>
+    <tr>
+        <td>3Dtechnologies R&D</td>
+        <td>müügiesindaja/projektijuht</td>
+        <td>1,5 aastat</td>
+    </tr>
+    <tr>
+        <td>Real Trade</td>
+        <td>finantsanalüütik</td>
+        <td>4 kuud</td>
+    </tr>
+    </tbody>
+    <div class=" h"> <h3>Valmis koduleht</h3></div>
+    <div class="pilt">
+        <a href="http://koverkiosk.ee"><img src="http://koverkiosk.ee/img/3ndbanner2013.png"/></a>
+    </div>
+    <h3>Ametikäik</h3>
+</table>
+</body>
+</html>
